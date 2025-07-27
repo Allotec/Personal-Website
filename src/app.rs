2f8f2865b47b02628f93dbb53f9d513dffc87266
Matthew Champagne <champagne7103@gmail.com>
2025-07-27 11:13:05 -0400

    Routing structure setup

--- src/app.rs
+++ src/app.rs
@@ -1,57 +1,71 @@
 use leptos::prelude::*;
 use leptos_meta::*;
+use leptos_router::{
+    StaticSegment,
+    components::{Route, Router, Routes},
+};
 
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
+        <Router>
+            <Routes fallback=|| PageNotFound>
+                <Route path=StaticSegment("") view=HomePage/>
+                <Route path=StaticSegment("/experience") view=ExperiencePage/>
+
+                //TODO: Implement these pages
+                <Route path=StaticSegment("/projects") view=PageNotFound/>
+                <Route path=StaticSegment("/about") view=PageNotFound/>
+                <Route path=StaticSegment("/contact") view=PageNotFound/>
+                <Route path=StaticSegment("/tools") view=PageNotFound/>
+            </Routes>
+        </Router>
+    }
+}
+
+#[component]
+fn PageNotFound() -> impl IntoView {
+    view! {
+        <p> "Page not Found" </p>
+    }
+}
+
+#[component]
+fn HomePage() -> impl IntoView {
+    view! {
         <div class="flex min-h-screen flex-col bg-stone-950 text-center font-fira-mono text-base text-gray-300">
             <div class="flex flex-1 flex-col items-center">
-                <Header />
-                <Home />
+                <SideBar />
+                <p class="m-auto w-3/4 md:text-xl">
+                    "This is a client-side rendering website template made with "
+                </p>
             </div>
         </div>
     }
 }
 
 #[component]
-fn Home() -> impl IntoView {
-    view! {
-        <div class="mx-4 flex flex-1 items-center pb-20">
-            <div class="mx-auto max-w-3xl space-y-14 text-center md:space-y-20">
-                <div>
-                    <h1 class="pb-6 text-lg font-bold text-rose-400 md:text-2xl">"Rust on the Client Side?!"</h1>
-                    <p class="m-auto w-3/4 md:text-xl">
-                        "This is a client-side rendering website template made with "
-                        <a class="text-rose-300 transition duration-200 hover:text-rose-200 active:text-rose-200"
-                            href="https://leptos.dev/" target="_blank">"Leptos"
-                        </a>
-                        ", "
-                        <a class="text-rose-300 transition duration-200 hover:text-rose-200 active:text-rose-200"
-                            href="https://trunkrs.dev/" target="_blank">"Trunk"</a>
-                        " and "
-                        <a class="text-rose-300 transition duration-200 hover:text-rose-200 active:text-rose-200"
-                            href="https://tailwindcss.com/" target="_blank">"Tailwind"</a>
-                    </p>
-                </div>
-                <div class="text-sm md:text-base">
-                    <p>"You can check the source code "
-                        <a class="border-b border-b-rose-300 text-rose-300 transition duration-200 hover:border-b-rose-200 hover:text-rose-200 active:border-b-rose-200 active:text-rose-200"
-                            href="https://github.com/lpnh/leptrun" target="_blank">"here"</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-    }
-}
-
-#[component]
-fn Header() -> impl IntoView {
+fn SideBar() -> impl IntoView {
     view! {
         <div
             class="inset-x-0 bottom-auto top-0 mt-4 flex items-center justify-center tracking-widest text-amber-200 sm:text-lg">
-            "Hello, friend!"
+            "Bye, Matt!"
+        </div>
+    }
+}
+
+#[component]
+fn ExperiencePage() -> impl IntoView {
+    view! {
+        <div class="flex min-h-screen flex-col bg-stone-950 text-center font-fira-mono text-base text-gray-300">
+            <div class="flex flex-1 flex-col items-center">
+                <SideBar />
+                <p class="m-auto w-3/4 md:text-xl">
+                    "This is an experience page"
+                </p>
+            </div>
         </div>
     }
 }
