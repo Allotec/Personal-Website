216512e56a9e6bc0eccafb95714ffb2ba25be1e6
Matthew Champagne <champagne7103@gmail.com>
2025-07-27 21:41:56 -0400

    Made skeleton

--- src/app.rs
+++ src/app.rs
@@ -16,13 +16,11 @@ pub fn App() -> impl IntoView {
         <Router>
             <Routes fallback=|| PageNotFound>
                 <Route path=StaticSegment("") view=HomePage/>
-                <Route path=StaticSegment("/experience") view=PageNotFound/>
-
-                //TODO: Implement these pages
-                <Route path=StaticSegment("/projects") view=PageNotFound/>
-                <Route path=StaticSegment("/about") view=PageNotFound/>
-                <Route path=StaticSegment("/contact") view=PageNotFound/>
-                <Route path=StaticSegment("/tools") view=PageNotFound/>
+                <Route path=StaticSegment("/experience") view=ExperiencePage/>
+                <Route path=StaticSegment("/projects") view=ProjectPage/>
+                <Route path=StaticSegment("/about") view=AboutPage/>
+                <Route path=StaticSegment("/contact") view=ContactPage/>
+                <Route path=StaticSegment("/tools") view=ToolsPage/>
             </Routes>
         </Router>
     }
@@ -42,7 +40,7 @@ fn HomePage() -> impl IntoView {
             <SideBar />
             <div class="flex flex-1 flex-col items-center bg-[rgb(20,22,23)] text-gray-200">
                 <p class="m-auto w-3/4 md:text-xl text-white-500">
-                    "Make a description about yourself"
+                    "Under Construction"
                 </p>
             </div>
         </div>
@@ -52,11 +50,67 @@ fn HomePage() -> impl IntoView {
 #[component]
 fn ExperiencePage() -> impl IntoView {
     view! {
-        <div class="flex min-h-screen flex-col bg-stone-950 text-center font-fira-mono text-base text-gray-300">
-            <div class="flex flex-1 flex-col items-center">
-                <SideBar />
-                <p class="m-auto w-3/4 md:text-xl">
-                    "This is an experience page"
+        <div class="flex h-screen">
+            <SideBar />
+            <div class="flex flex-1 flex-col items-center bg-[rgb(20,22,23)] text-gray-200">
+                <p class="m-auto w-3/4 md:text-xl text-white-500">
+                    "Under Construction"
+                </p>
+            </div>
+        </div>
+    }
+}
+
+#[component]
+fn ProjectPage() -> impl IntoView {
+    view! {
+        <div class="flex h-screen">
+            <SideBar />
+            <div class="flex flex-1 flex-col items-center bg-[rgb(20,22,23)] text-gray-200">
+                <p class="m-auto w-3/4 md:text-xl text-white-500">
+                    "Under Construction"
+                </p>
+            </div>
+        </div>
+    }
+}
+
+#[component]
+fn AboutPage() -> impl IntoView {
+    view! {
+        <div class="flex h-screen">
+            <SideBar />
+            <div class="flex flex-1 flex-col items-center bg-[rgb(20,22,23)] text-gray-200">
+                <p class="m-auto w-3/4 md:text-xl text-white-500">
+                    "Under Construction"
+                </p>
+            </div>
+        </div>
+    }
+}
+
+#[component]
+fn ContactPage() -> impl IntoView {
+    view! {
+        <div class="flex h-screen">
+            <SideBar />
+            <div class="flex flex-1 flex-col items-center bg-[rgb(20,22,23)] text-gray-200">
+                <p class="m-auto w-3/4 md:text-xl text-white-500">
+                    "Under Construction"
+                </p>
+            </div>
+        </div>
+    }
+}
+
+#[component]
+fn ToolsPage() -> impl IntoView {
+    view! {
+        <div class="flex h-screen">
+            <SideBar />
+            <div class="flex flex-1 flex-col items-center bg-[rgb(20,22,23)] text-gray-200">
+                <p class="m-auto w-3/4 md:text-xl text-white-500">
+                    "Under Construction"
                 </p>
             </div>
         </div>
