a670c3175255edbf00912cfac99d3ceaa2c14fd2
Matthew Champagne <champagne7103@gmail.com>
2025-08-16 23:54:12 -0400

    Filled in the content for the projects page

--- src/app/project.rs
+++ src/app/project.rs
@@ -36,44 +36,96 @@ impl ProjectInfo {
 pub(crate) fn ProjectPageContent() -> impl IntoView {
     let projects = vec![
         ProjectInfo::new(
-            "images/screenshot.jpg",
-            "Project Title",
-            "This is a project description",
-            vec!["Visit".to_string(), "Github".to_string()],
-            "https://google.com",
+            "images/personal_website.png",
+            "Portfolio Website",
+            "Website used to host my personal experience and socials.",
+            vec![
+                "Rust".to_string(),
+                "Leptos".to_string(),
+                "Web Dev".to_string(),
+            ],
+            "https://github.com/Allotec/Personal-Website",
             true,
         ),
         ProjectInfo::new(
-            "images/screenshot.jpg",
-            "Project Title",
-            "This is a project description",
-            vec!["Visit".to_string(), "Github".to_string()],
-            "https://google.com",
+            "images/neovim_config.png",
+            "Neovim Configuration",
+            "Personal Neovim configuration based on Astronvim.",
+            vec!["Lua".to_string(), "Editor".to_string()],
+            "https://github.com/Allotec/nvim-config",
             true,
         ),
         ProjectInfo::new(
-            "images/screenshot.jpg",
-            "Project Title",
-            "This is a project description",
-            vec!["Visit".to_string(), "Github".to_string()],
-            "https://google.com",
+            "images/cc_tweaked.png",
+            "CC:Tweaked Programs",
+            "Some fun turtle farming programs for CC:Tweaked.",
+            vec!["Lua".to_string(), "Minecraft".to_string()],
+            "https://github.com/Allotec/ComputerCraftPrograms",
             true,
         ),
         ProjectInfo::new(
-            "images/screenshot.jpg",
-            "Project Title",
-            "This is a project description",
-            vec!["Visit".to_string(), "Github".to_string()],
-            "https://google.com",
-            true,
+            "images/senior_project.png",
+            "Senior Project",
+            "Neural network library build on top of the eigen C++ matrix library.",
+            vec!["C++".to_string(), "AI".to_string()],
+            "https://github.com/Allotec/Senior-Project",
+            false,
         ),
         ProjectInfo::new(
-            "images/screenshot.jpg",
-            "Project Title",
-            "This is a project description",
-            vec!["Visit".to_string(), "Github".to_string()],
-            "https://google.com",
-            true,
+            "images/ai_steth.png",
+            "AI Stethoscope",
+            "Used the sounds from a digital stethoscope to train an AI classifier for various medical conditions.",
+            vec!["Python".to_string(), "AI".to_string()],
+            "https://github.com/Allotec/AI-Stethoscope",
+            false,
+        ),
+        ProjectInfo::new(
+            "images/nas_network.png",
+            "NAS Monitor Bot",
+            "Automation script written for my NAS to send me status messages over discord about the health of the disk array.",
+            vec!["Rust".to_string(), "Discord".to_string()],
+            "https://github.com/Allotec/NAS-Monitor-BOT",
+            false,
+        ),
+        ProjectInfo::new(
+            "images/sony_spu.png",
+            "Sony SPU",
+            "Implementation of the Sony SPU (Vector Processor) for the Sony PlayStation 3.",
+            vec!["VHDL".to_string(), "Python".to_string()],
+            "https://github.com/Allotec/Sony_SPU",
+            false,
+        ),
+        ProjectInfo::new(
+            "images/pipelined_cpu.png",
+            "Pipelined CPU",
+            "My first CPU implementation done for my undergraduate computer architecture class.",
+            vec!["VHDL".to_string(), "Python".to_string()],
+            "https://github.com/Allotec/Pipelined-CPU",
+            false,
+        ),
+        ProjectInfo::new(
+            "images/assembler.png",
+            "MIPS Assembler",
+            "Very old MIPS assembler written in C++ over a summer for fun.",
+            vec!["C++".to_string(), "Assembly".to_string()],
+            "https://github.com/Allotec/MIPS-Assembler",
+            false,
+        ),
+        ProjectInfo::new(
+            "images/disassembler.png",
+            "MIPS Disassembler",
+            "Very old MIPS disassembler written in C++ over a summer for fun.",
+            vec!["C++".to_string(), "Assembly".to_string()],
+            "https://github.com/Allotec/MIPS-Disassembler",
+            false,
+        ),
+        ProjectInfo::new(
+            "images/scoreboard.png",
+            "Arduino Scoreboard",
+            "Scoreboard application for a local tabletop gaming group. Made from an Arduino and a 4 digit seven segment display.",
+            vec!["C++".to_string(), "Arduino".to_string()],
+            "https://github.com/Allotec/Arduino-Scoreboard",
+            false,
         ),
     ];
 
@@ -125,13 +177,15 @@ fn ProjectTile(
 ) -> impl IntoView {
     view! {
         <a href={host_site}>
-        <div class="flex flex-col bg-[rgb(28,30,32)] rounded-lg p-2 max-w-md max-h-80 overflow-hidden hover:scale-103">
-            <img src=img_path class="flex rounded-lg w-full object-cover mb-2" />
-            <div class="flex text-white-100 text-base font-semibold">
-                {title}
-            </div>
-            <div class="flex text-stone-300 text-sm">
-                {description}
+        <div class="flex flex-col bg-[rgb(28,30,32)] rounded-lg p-2 max-w-md h-full overflow-hidden hover:scale-103 justify-between">
+            <div>
+                <img src=img_path class="flex rounded-lg w-full object-cover mb-2" />
+                <div class="flex text-white-100 text-base font-semibold">
+                    {title}
+                </div>
+                <div class="flex text-stone-300 text-sm">
+                    {description}
+                </div>
             </div>
             <div class="flex flex-row mt-2 space-x-2 justify-between items-center">
                 <div class="flex space-x-2">
@@ -139,8 +193,7 @@ fn ProjectTile(
                         <TileTag text=text.clone() />
                     }).collect::<Vec<_>>()}
                 </div>
-
-            <StatusTag is_active=is_active />
+                <StatusTag is_active=is_active />
             </div>
         </div>
         </a>
